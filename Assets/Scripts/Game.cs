73ae768f90a9cde1f833d95664ac43cdfa2fed69
Tyler Mitchell <tylermitchellisawesome@gmail.com>
2013-04-27 12:37:21 -0400

    mess with splines

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Game : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -10,10 +10,6 @@ public class NewBehaviourScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	//poop
-		//testing...
-		//daskj
-		
-		//Hi Pat
+	
 	}
 }
