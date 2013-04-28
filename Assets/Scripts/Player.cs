9c954d403ada1261fecc3192b0dd55e3d28abce0
Patrick Galagan <pmgalagan@gmail.com>
2013-04-28 15:39:57 -0400

    Integrating signs with nodes

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//this.gameObject.transform.position = GameObject.Find("Begin");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Sign.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+
+public enum SignType
+{
+    None, Stop, OneWayLeft, OneWayRight, Speed15, Speed30
+}
+
+public class Sign : MonoBehaviour {
+
+    private GUISigns guiSignRef;
+    public PathNode pathNodeRef;
+
+    public SignType type = SignType.None;
+
+	// Use this for initialization
+	void Start () {
+         guiSignRef = GameObject.Find("GameController").GetComponent<GUISigns>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnMouseDown()
+    {
+        guiSignRef.updateSignReference(this);
+    }
+
+    public void updateSignType(SignType newType)
+    {
+        type = newType;
+        string modelpath;
+        GameObject go = null;
+
+        switch (type)
+        {
+            case SignType.OneWayLeft:
+                modelpath = "oneWaySign";
+                go = Instantiate(Resources.Load(modelpath)) as GameObject;
+                break;
+        }
+
+        MeshFilter mf1 = (MeshFilter)go.GetComponent("MeshFilter");
+        Mesh m1 = (Mesh)Mesh.Instantiate(mf1.mesh);
+        MeshFilter mf2 = (MeshFilter)GetComponent("MeshFilter");
+        mf2.mesh = m1;
+
+    }
+}
