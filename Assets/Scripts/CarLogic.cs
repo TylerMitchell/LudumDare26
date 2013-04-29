1f726a451a85bae41f92e2ab661624af3583b5ad
Patrick Galagan <pmgalagan@gmail.com>
2013-04-29 01:49:36 -0400

    hooked up the signs

--- Assets/Scripts/CarLogic.cs
+++ Assets/Scripts/CarLogic.cs
@@ -6,14 +6,17 @@ public class CarLogic : MonoBehaviour {
 	public PathNode betweenA;
 	public PathNode betweenB;
 	public float progress = 0.0f;
+	public float baseSpeed = 0.01f;
+	public float ds;
 	// Use this for initialization
 	void Start () {
 		//GameObject.Find ("GameController").GetComponent<Game>().PutAtNode(this, start);
+		ds = baseSpeed * 1/Vector3.Magnitude(betweenB.transform.position - betweenA.transform.position);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(progress < 1.0f){ progress += 0.05f; }
+		if(progress < 1.0f){ progress += ds; }
 		transform.position = Vector3.Lerp(betweenA.transform.position, betweenB.transform.position, progress);
 	}
 	
